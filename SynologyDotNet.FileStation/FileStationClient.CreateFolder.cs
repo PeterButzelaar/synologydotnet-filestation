c7d434ba6c412bc213ea1ee5ab702581f90c44f3
Peter Butzelaar <peter.butzelaar@outlook.com>
2022-08-01 10:31:43 +0200

    df

--- SynologyDotNet.FileStation/FileStationClient.CreateFolder.cs
+++ SynologyDotNet.FileStation/FileStationClient.CreateFolder.cs
@@ -12,7 +12,11 @@ namespace SynologyDotNet.FileStation
         /// <summary>
         /// Create folders on File Station.
         /// </summary>
+        /// <param name="folders">One or more shared folder paths, separated by commas and around brackets.If force_parent is "true," and folder_path does not exist, the folder_path will be created.If force_parent is "false," folder_path must exist or a false value will be returned. The number of paths must be the same as the number of names in the name parameter.The first folder_path parameter corresponds to the first name parameter.</param>
+        /// <param name="forceParent">Optional. true : no error occurs if a folder exists and create parent folders as needed. false : parent folders are not created..</param>
+        /// <param name="additionalFields">The additional fields.</param>
         /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">folders</exception>
         public Task<ApiDataResponse<FolderList>> CreateFolderAsync((string folderPath, string name)[] folders, bool forceParent = false, QueryAdditional additionalFields = QueryAdditional.All)
         {
             if (folders == null || !folders.Any())
